07fc89ec40e62c0690ae38f73d81fcf959eb60f5
Waiz <164471173+decodewidwaiz@users.noreply.github.com>
2025-01-05 16:00:40 +0530

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -4,4 +4,5 @@ qr = qrcode.QRCode(version=1, box_size=5, border=5)
 qr.add_data(website_url)
 qr.make()
 img = qr.make_image(fill_color='black', back_color = 'white')
+#saved as png form.
 img.save('website_link.png')
