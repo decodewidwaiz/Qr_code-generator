d33513cdadf5946de04228c399dd9ef071ebdda8
Waiz <164471173+decodewidwaiz@users.noreply.github.com>
2025-01-16 10:36:47 +0530

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -3,6 +3,7 @@ website_url ="linktr.ee/Waizzzz69"
 qr = qrcode.QRCode(version=1, box_size=5, border=5)
 qr.add_data(website_url)
 qr.make()
+
 img = qr.make_image(fill_color='black', back_color = 'white')
 #saved as png form ok
 img.save('website_link.png')
